79acec07e5ffd65366779f1d79d741dd1f1f9741
Shipra Kumari <skumari@veracode.com>
2026-04-29 10:20:45 +0530

    Removing vulneribilities

--- src/services/workflow-service.js
+++ src/services/workflow-service.js
@@ -127,7 +127,22 @@ async function executePolicyScan(vid, vkey, veracodeApp, jarName, version, filep
   let stderr;
   try {
     core.info(`Command to execute the policy scan : ${policyScanCommand}`);
-    stdout = execFileSync("java",args, { encoding: "utf-8" });
+    stdout = execFileSync("java",[
+      "-jar", jarName,
+      "-action", "UploadAndScanByAppId",
+      "-vid", vid,
+      "-vkey", vkey,
+      "-appid", veracodeApp.appId,
+      "-filepath", filepath,
+      "-version,", version,
+      '-scanpollinginterval', '30', 
+      '-autoscan', 'true', 
+      '-scanallnonfataltoplevelmodules', 'true', 
+      '-includenewmodules', 'true', 
+      '-scantimeout', '6000', 
+      '-deleteincompletescan', '2',
+      debugFlag
+    ], { encoding: "utf-8" });
   } catch (error) {
     stdout = error.stdout?.toString();
     stderr = error.stderr?.toString();
